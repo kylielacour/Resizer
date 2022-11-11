5af38e09dcb3212868c2d6fcb2b889b3fef65a6d
Kylie LaCour <kylielacour@gmail.com>
2022-11-10 19:24:47 -0800

    clayton helped

--- code.ts
+++ code.ts
@@ -8,28 +8,44 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-//function to get folder name prefix
-const getNameParts = (name: string) => {
-  const nameParts = name.split('/').filter((part: string) => !!part)
-  return nameParts.map((part: string) => part.trim())
-}
-const getNamePrefix = (name: string): string => {
-  const pathParts = getNameParts(name)
-  pathParts.pop()
-  return pathParts.join('/')
-}
-
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = msg => {
   if (msg.type === 'spacer-input') {
 
+
+    // get the new folder name from the user
+    const input = msg.spacerInput;
+
+    //function to get folder name suffix
+    const getNameParts = (name: string) => {
+    const nameParts = name.split('/').filter((part: string) => !!part)
+    return nameParts.map((part: string) => part.trim())
+    }
+    const getNameSuffix = (name: string): string => {
+    const pathParts = getNameParts(name)
+    pathParts.shift()
+    return pathParts[0]
+    }
+
+    //function to add folder as a prefix
+    const addFolder = (name: string): string => {
+    const pathParts = getNameParts(name)
+    pathParts.unshift(input)
+    return pathParts.join('/')
+    }
+
+    if (figma.currentPage.selection.length === 0) {
+      return; //nothing is selected
+    }
+
     //Skip over invisible nodes and their descendants inside instances for faster performance
     figma.skipInvisibleInstanceChildren = true
 
-    //Gets all instance and text nodes
-    const components = figma.currentPage.findAllWithCriteria({
+    //Gets all selected instance and text nodes
+    // @ts-ignore 
+    const components = figma.currentPage.selection[0].findAllWithCriteria({
       types: ['INSTANCE', 'TEXT']
     })
 
@@ -48,39 +64,31 @@ figma.ui.onmessage = msg => {
       }
     }
 
-    // get the new folder name from the user
-    const input = msg.spacerInput; // 768
-
     // loop through text nodes
-    // for each text node, get the full name
-    // discard the folder portion and keep the name
-    // using the new folder name from the user, create a string in the form of folder/name
-    // assign it to the node
-
+    for (let text of textNodes){
+      if (text.textStyleId){
+        // for each text node, get the full name
+        const fullName = (figma.getStyleById(text.textStyleId) as TextStyle).name;
+        // discard the folder portion and keep the name
+        const styleName = getNameSuffix(fullName);
+        // using the new folder name from the user, create a string in the form of folder/name
+        const newGroup = addFolder(styleName);
+        // get all local styles in document
+        const allStyles = figma.getLocalTextStyles()
+        // search through styles to find one that matches newGroup name
+        const matchingStyle = allStyles.find(style => style.name === newGroup)
+        // change id of text node to matchingStyle id
+        text.textStyleId = matchingStyle?.id
+      }
+    }
     //Finds all nodes in spacerNodes array with the name ~spacer and changes the Variant Property
     for (const spacer of spacerNodes) {
       spacer.setProperties({"Comp":input})
     }
-
-  }
-
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
   }
 
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  // figma.closePlugin();
 };
